613ea0485fa45eac32eb97940aabe004171b9a4b
Andrej Smigala <asmigala@redhat.com>
2015-08-05 10:48:54 +0200

    Teiid: fix ImportWizardTest

--- plugins/org.jboss.tools.teiid.reddeer/src/org/jboss/tools/teiid/reddeer/editor/ModelEditor.java
+++ plugins/org.jboss.tools.teiid.reddeer/src/org/jboss/tools/teiid/reddeer/editor/ModelEditor.java
@@ -75,12 +75,12 @@ public class ModelEditor extends SWTBotEditor {
 	}
 
 	private GraphicalEditor getGraphicalEditor(String tabLabel) {
-		final SWTBotCTabItem tabItem = showTab(tabLabel);
+		final CTabItem tabItem = showTab(tabLabel);
 		GraphicalEditor graphicalEditor = syncExec(new Result<GraphicalEditor>() {
 
 			@Override
 			public GraphicalEditor run() {
-				Object obj = tabItem.widget.getData();
+				Object obj = tabItem.getSWTWidget().getData();
 				if (obj instanceof GraphicalEditor) {
 					return (GraphicalEditor) obj;
 				}
@@ -119,10 +119,9 @@ public class ModelEditor extends SWTBotEditor {
 		return tabItem;
 	}
 
-	public SWTBotCTabItem showTab(String label) {
-		SWTBotCTabItem tabItem = bot.cTabItem(label);
+	public CTabItem showTab(String label) {
+		DefaultCTabItem tabItem = new DefaultCTabItem(label);
 		tabItem.activate();
-		tabItem.show();
 		return tabItem;
 	}
 	
